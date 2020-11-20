904680fdb66233856d82f80d924cfe4a4455413f
Spencer Pease <spencer.p.816@gmail.com>
2020-11-19 23:41:04 -0800

    feat: Add example function
    This function acts a a reference and a test for the package
    infrastructure.

---
+++ R/example.R
@@ -0,0 +1,25 @@
+#' Example Addition Function Title
+#'
+#' Description of example addition function. Here we put a short paragraph
+#' describing what the function does at a high level.
+#'
+#' @param a `numeric()` First number to add
+#' @param b `numeric()` Second number to add
+#'
+#' @return The addition of `a` and `b`
+#'
+#' @export
+#'
+#' @examples
+#' example_add(1, 2)
+#' example_add(5.4, 3.2)
+#' example_add(3.3, 1:10)
+#'
+#' \dontrun{
+#' example_add("a", "b")
+#' }
+example_add <- function(a, b) {
+
+  a + b
+
+}
