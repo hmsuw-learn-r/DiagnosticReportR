c4103420bc517b8f397df0bfb2c852cab0081164
Madeline Moberg <mobergm@uw.edu>
2020-12-15 12:38:59 -0800

    edits to plotting examples

--- R/patterns.R
+++ R/patterns.R
@@ -2,18 +2,19 @@
 #' 
 #' This function can be used to visualize the results of the rank_change function, or to otherwise 
 #' compare values from two different model versions or sets of estimates. This can help streamline 
-#' the vetting process. All characteristics for each estimate should be matched, e.g. year, location, 
-#' age. plot_trends creates plots of the estimates over some unit of time comparing the two sets
-#' of results. This could be used for time trends across years, or age patterns. The options for faceting 
-#' or coloring allow the user to customize the plot to maximize utility. This function assumes that the
-#' two sets of estimates are in two different columns, e.g. old_mean and new_mean, and uses pivot_longer
-#' from tidyr to reshape the data long to make plotting more straightforward and flexible. 
+#' the vetting process. All characteristics for each estimate should be matched, e.g. year, 
+#' location, age. plot_trends creates plots of the estimates over some unit of time comparing the
+#' two sets of results. This could be used for time trends across years, or age patterns. The 
+#' options for faceting or coloring allow the user to customize the plot to maximize utility. This 
+#' function assumes that the two sets of estimates are in two different columns, e.g. old_mean and
+#' new_mean, and uses pivot_longer from tidyr to reshape the data long to make plotting more
+#' straightforward and flexible. 
 #'
 #' @param data data.table or data.frame. Data to plot comparing two sets of estimates, e.g. 
 #' output of rank_change function. Required!
 #' @param time_var `character()` Variable name for time, e.g. year/year_id or ages. Required!
-#' @param y1_var `character()` Variable name for first set of results/estimates, e.g. the previous or
-#'  old set for comparison. Required!
+#' @param y1_var `character()` Variable name for first set of results/estimates, e.g. the previous
+#' or old set for comparison. Required!
 #' @param y2_var `character()` Variable name for second set of results/estimates, e.g. the updated 
 #' or new set for comparison. Required!
 #' @param facet_x `character()` Variable to facet by, along x-axis. This is required in order to use 
@@ -21,25 +22,37 @@
 #' @param facet_y `character()` Variable to facet by, along y-axis, if desired. This is required in 
 #' order to use facet_grid. Defaults to NULL.
 #' @param title `character()` Title of plot, if desired. Defaults to NULL.
-#' @param facet_type `character()` Choice of "none", "wrap", or "grid". Defaults to "none". The "none"
-#' option will create a single plot. The "wrap" option will produce a plot using facet_wrap. The "grid" 
-#' option will produce a plot using facet_grid. Required!
+#' @param facet_type `character()` Choice of "none", "wrap", or "grid". Defaults to "none". The 
+#' "none" option will create a single plot. The "wrap" option will produce a plot using facet_wrap.
+#' The "grid" option will produce a plot using facet_grid. Required!
 #' @param colors Vector. This vector would contain the name of two colors to be used in the plot 
 #' to differentiate the two versions or sets of values. This defaults to dodgerblue and deeppink4, 
 #' but can be otherwise specified by the user.
-#' @param my_labs Vector. This vector would contain the labels/names you want to use for the two different sets of 
-#' estimates. If left blank this will default to the column names for y1_var and y2_var. 
+#' @param my_labs Vector. This vector would contain the labels/names you want to use for the two 
+#' different sets of estimates. If left blank this will default to the column names for y1_var 
+#' and y2_var. 
 #' @param line_size `numeric()` Can select line thickness. Default is 1. 
 #'
 #' @return A ggplot object. 
 #' 
 #' @import tidyr 
 #' @import ggplot2
+
 #' 
 #' @export
 #'
 #' @examples
-#' \dontrun{plot_trends(trend_changes, time_var = "year_id", y1_var = "old_mean", y2_var = "new_mean", colors = c("blue", "red"))}
+#' \dontrun{
+#' plot_trends(trend_changes, 
+#'             time_var = "year_id", 
+#'             y1_var = "old_mean", 
+#'             y2_var = "new_mean", 
+#'             facet_x = "ihme_loc_id",
+#'             colors = c("blue", "red"),
+#'             my_labs = c("previous","new"),
+#'             facet_type = "wrap",
+#'             line_size = 2)
+#'             }
 plot_trends <- function(data, 
                         time_var,
                         y1_var, 
@@ -87,7 +100,8 @@ plot_trends <- function(data,
   }
   
   else {
-    stop("Are all variable names character strings? Did you specify your facet_type correctly, based on facet_x and facet_y?")
+    stop("Are all variable names character strings? 
+         Did you specify your facet_type correctly, based on facet_x and facet_y?")
   }
   
 }
--- R/scatters.R
+++ R/scatters.R
@@ -1,22 +1,31 @@
 #' plot_scatter
 #' 
-#' This function can be used to visualize the results of the rank_change function, or to otherwise compare values from two 
-#' different model versions or sets of estimates. This can help streamline the vetting process. All characteristics for each estimate 
-#' should be matched, e.g. year, location, age. Plot_scatter creates scatter plots comparing the two sets of results. The various
-#' options for faceting or coloring the points allow the user to customize the plot to maximize utility. 
+#' This function can be used to visualize the results of the rank_change function, or to otherwise 
+#' compare values from two different model versions or sets of estimates. This can help streamline 
+#' the vetting process. All characteristics for each estimate should be matched, e.g. year, 
+#' location, age. Plot_scatter creates scatter plots comparing the two sets of results. The 
+#' various options for faceting or coloring the points allow the user to customize the plot to 
+#' maximize utility. 
 #'
-#'
-#' @param data data.frame or data.table. Data to plot comparing two sets of estimates, e.g. output of rank_change function. Required!
-#' @param x_var `character()` Variable to plot on the x-axis, e.g the "old" version or previous estimate. Required!
-#' @param y_var `character()` Variable to plot on the y-axis, e.g. the "new" version or updated estimate. Required!
-#' @param facet_x `character()` Variable to facet by, along x-axis. This is required in order to use facet_wrap and facet_grid. Defaults to NULL.
-#' @param facet_y `character()` Variable to facet by, along y-axis, if desired. This is required in order to use facet_grid. Defaults to NULL.
-#' @param color_pt `character()` Variable to modify color of geom_point, if desired. Defaults to NULL.
+#' @param data data.frame or data.table. Data to plot comparing two sets of estimates, e.g. 
+#' output of rank_change function. Required!
+#' @param x_var `character()` Variable to plot on the x-axis, e.g the "old" version or 
+#' previous estimate. Required!
+#' @param y_var `character()` Variable to plot on the y-axis, e.g. the "new" version or 
+#' updated estimate. Required!
+#' @param facet_x `character()` Variable to facet by, along x-axis. This is required in order
+#' to use facet_wrap and facet_grid. Defaults to NULL.
+#' @param facet_y `character()` Variable to facet by, along y-axis, if desired. This is required 
+#' in order to use facet_grid. Defaults to NULL.
+#' @param color_pt `character()` Variable to modify color of geom_point, if desired. 
+#' Defaults to NULL.
 #' @param pt_size `numeric()` Option to modify point size. Defaults to 1. 
 #' @param title `character()` Title for plot, if desired. Defaults to NULL.
-#' @param facet_type `character()` Choice of "none", "wrap", or "grid". Defaults to "none". The "none" option will create a single plot. 
-#' The "wrap" option will produce a plot using facet_wrap. The "grid" option will produce a plot using facet_grid. 
-#' @param fix_scale `boolean()` Option to fix the axes for plot, using the min and max values. Defaults to FALSE. Will override scales = "free".
+#' @param facet_type `character()` Choice of "none", "wrap", or "grid". Defaults to "none". The
+#' "none" option will create a single plot. The "wrap" option will produce a plot using facet_wrap. 
+#' The "grid" option will produce a plot using facet_grid. 
+#' @param fix_scale `boolean()` Option to fix the axes for plot, using the min and max values. 
+#' Defaults to FALSE. Will override scales = "free".
 #'
 #' @return A ggplot object
 #' 
@@ -25,7 +34,13 @@
 #' @export
 #'
 #' @examples 
-#' \dontrun{plot_scatter(data = level_changes, x_var = "old_mean", y_var = "new_mean", facet_x = "ihme_loc_id", color_pt = "year_id", facet_type = "wrap")}
+#' \dontrun{
+#' plot_scatter(data = level_changes, 
+#'              x_var = "old_mean", 
+#'              y_var = "new_mean", 
+#'              facet_x = "ihme_loc_id", 
+#'              color_pt = "year_id", 
+#'              facet_type = "wrap")}
 plot_scatter <- function(data, 
                          x_var, 
                          y_var, 
@@ -71,13 +86,16 @@ plot_scatter <- function(data,
   }
   
   else {
-    stop("Are all variable names character strings? Did you specify your facet_type correctly, based on facet_x and facet_y?")
+    stop("Are all variable names character strings? 
+         Did you specify your facet_type correctly, based on facet_x and facet_y?")
   }
   
   if (fix_scale == TRUE){
     my_scatter <- my_scatter + 
-                    scale_x_continuous(limits = c(min(data[, ..x_var], data[, ..y_var]), max(data[, ..x_var], data[, ..y_var]))) +
-                    scale_y_continuous(limits = c(min(data[, ..x_var], data[, ..y_var]), max(data[, ..x_var], data[, ..y_var]))) 
+      scale_x_continuous(limits = c(min(data[, ..x_var], data[, ..y_var]), 
+                                    max(data[, ..x_var], data[, ..y_var]))) +
+      scale_y_continuous(limits = c(min(data[, ..x_var], data[, ..y_var]), 
+                                    max(data[, ..x_var], data[, ..y_var]))) 
   }
   
   return(my_scatter)
